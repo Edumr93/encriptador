2d070410ee5a230071bd4eed6c20984aa4177c51
Edumr93 <92550253+Edumr93@users.noreply.github.com>
2022-09-05 09:04:08 -0300

    Actualizacion

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const areaUno = document.getElementById("areaUno");
 const areaDos = document.getElementById("areaDos");
+const copiado = document.getElementById("copiado");
+const vocales = [["e" , "enter"], ["i" , "imes"],["a" , "ai"], ["o", "ober"], ["u", "ufat"]];
 
+copiado.classList.add("ocultar");
 areaUno.focus();
 
 
 function encriptar(){
-    let textoAreaUno = "";
-    textoAreaUno = areaUno.value;
-    let vocales = [["e" , "enter"], ["i" , "imes"],["a" , "ai"], ["o", "ober"], ["u", "ufat"]];
-    let textoEncriptado = "";
-
+    areaDos.value = "";
+    let texto = "";
+    texto = areaUno.value;
 
     for(let i = 0; i < 5; i++){
-        if(textoAreaUno.includes(vocales[i][0])){
+        if(texto.includes(vocales[i][0])){
 
-            textoAreaUno = textoAreaUno.replaceAll(vocales[i][0], vocales[i][1]);
+            texto = texto.replaceAll(vocales[i][0], vocales[i][1]);
 
         }
     }
 
+    areaDos.value = texto;
     alert(textoAreaUno);
+}
 
+function desEncriptar(){
+    areaDos.value = "";
+    let texto = areaUno.value;
 
+    for(let i = 0; i < 5; i++){
+        if(texto.includes(vocales[i][1])){
+            texto = texto.replaceAll(vocales[i][1], vocales[i][0]);
+        }
+    }
+
+    areaDos.value = texto;
+}
+
+function copiar(){
+    copiado.classList.remove("ocultar");
+    navigator.clipboard.writeText(areaDos.value);
+    
 }
